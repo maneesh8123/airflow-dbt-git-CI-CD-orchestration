be56c8213a312e5022682d1f0ba46e767031adf2
Maneesh <64775171+maneesh8123@users.noreply.github.com>
2026-06-03 11:25:21 +0530

    Update dbt_transform.py

--- dags/dbt_transform.py
+++ dags/dbt_transform.py
@@ -9,7 +9,7 @@ default_args = {
     'start_date': datetime(2023, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
-    # 'email': ['maneesh33a@gmail.com'],
+    # 'email': ['youremail'],
     # 'email_on_failure': True,
     # 'email_on_retry': False,
 }
